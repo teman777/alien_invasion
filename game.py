898dbd7e5c63cc6833dbbe6f775ab013394db947
Воронов Артем Игоревич <voronovai@intranet.vdcom>
2020-01-24 16:52:54 +0400

    Tupa create new fucking shit

---
+++ game.py
@@ -0,0 +1,25 @@
+import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
+from pygame.sprite import Group
+
+
+
+def run_game():
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_settings,ship,screen, aliens)
+    pygame.display.set_caption("Alien Invasion")
+    while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(bullets,aliens)
+        gf. update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, bullets, aliens)
+
+run_game()
